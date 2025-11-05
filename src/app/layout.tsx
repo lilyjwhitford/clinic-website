f905babec254e3ee30b6bc7d6c0d525b12a2c61f
Lily Whitford <lilywhitford2@gmail.com>
2025-11-05 13:34:33 -0800

    install heroui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
+import Providers from './providers'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -28,7 +29,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Navbar />
-        <main>{children}</main>
+        <Providers>{children}</Providers>
         <Footer />
       </body>
     </html>
