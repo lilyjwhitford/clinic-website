ef68f30ddaa6677c9686435ea04d6f4aa432f4de
Lily Whitford <lilywhitford2@gmail.com>
2025-11-10 13:15:52 -0800

    add props to HoverDropdown function for reusability in NavBar, and add it to PRP

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,7 +15,14 @@ import {
 // import { useScroll } from 'framer-motion'
 import Link from 'next/link'
 
-function HoverDropdown() {
+// function that uses state to handle hover dropdown menu
+function HoverDropdown({
+  label,
+  items,
+}: {
+  label: string
+  items: { key: string; href: string; text: string }[]
+}) {
   const [isOpen, setIsOpen] = useState(false)
 
   const handleMouseEnter = () => {
@@ -30,26 +37,19 @@ function HoverDropdown() {
     <Dropdown isOpen={isOpen} onOpenChange={setIsOpen}>
       <DropdownTrigger onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
         <Button variant="light" className="font-medium">
-          Services
+          {label}
         </Button>
       </DropdownTrigger>
       <DropdownMenu
-        aria-label="Services Menu"
+        aria-label={`${label} menu`}
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
       >
-        <DropdownItem key="Botox">
-          <Link href="/services#Botox">Botox</Link>
-        </DropdownItem>
-        <DropdownItem key="Filler">
-          <Link href="/services#Filler">Filler</Link>
-        </DropdownItem>
-        <DropdownItem key="PlexR Treatments">
-          <Link href="/services#PlexR-Treatments">PlexR Treatments</Link>
-        </DropdownItem>
-        <DropdownItem key="Venus Versa">
-          <Link href="/services#Venus-Versa">Venus Versa</Link>
-        </DropdownItem>
+        {items.map((item) => (
+          <DropdownItem key={item.key}>
+            <Link href={item.href}>{item.text}</Link>
+          </DropdownItem>
+        ))}
       </DropdownMenu>
     </Dropdown>
   )
@@ -69,7 +69,28 @@ export default function NavBar() {
           <Link href="/">home</Link>
         </NavbarItem>
         <NavbarItem>
-          <HoverDropdown />
+          <HoverDropdown
+            label="services"
+            items={[
+              { key: 'Botox', href: 'services#Botox', text: 'Botox' },
+              { key: 'Filler', href: '/services#Filler', text: 'Filler' },
+              {
+                key: 'PlexR Treatments',
+                href: '/services#PlexR-Treatments',
+                text: 'PlexR Treatments',
+              },
+              { key: 'Venus Versa', href: '/services#Venus-Versa', text: 'Venus Versa' },
+            ]}
+          />
+        </NavbarItem>
+        <NavbarItem>
+          <HoverDropdown
+            label="PRP"
+            items={[
+              { key: 'Medical', href: '/prp#medical', text: 'Medical' },
+              { key: 'Aesthetic', href: '/prp#aesthetic', text: 'Aesthetic' },
+            ]}
+          />
         </NavbarItem>
         <NavbarItem>
           <Link href="/about">about</Link>
